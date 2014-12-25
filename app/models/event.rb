29707f39ce8d607244e95e14253d0fb3fe556554
ColbyCheeZe <colby@colbycheeze.com>
2014-12-25 21:16:49 +0000

    Sort of workign...ish

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   belongs_to :creator, class_name: "User", foreign_key: :user_id
-  has_many :invitations
+  has_many :invitations, foreign_key: :event_id
   has_many :attendees, through: :invitations
 end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :events
-  has_many :invitations, foreign_key: :event_id
+  has_many :invitations, foreign_key: :user_id
   has_many :attended_events, through: :invitations
 end
 
